d08a7aa212ca843d67845718020c38a42640d384
stuibeal <github@a.stuiber.net>
2023-07-22 14:42:56 +0200

    initial

---
+++ ZTemperatur/ZTemperatur.h
@@ -0,0 +1,23 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+// https://github.com/stm32duino/BoardManagerFiles/raw/main/package_stmicroelectronics_index.json
+
+#ifndef _ZTemperatur_H_
+#define _ZTemperatur_H_
+#include "Arduino.h"
+//add your includes for the project ZTemperatur here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project ZTemperatur here
+
+
+
+
+//Do not add code below this line
+#endif /* _ZTemperatur_H_ */
---
+++ ZTemperatur/spec.d
@@ -0,0 +1,2 @@
+spec.o: \
+ /home/alfred3/eclipse-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.cpp
